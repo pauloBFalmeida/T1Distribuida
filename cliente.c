99de0daa904a6f5a5472e780f1b6524de7c290f9
pauloBFalmeida <paulobfalmeida@gmail.com>
2021-04-05 19:12:39 -0300

    cliente bonito

--- cliente.c
+++ cliente.c
@@ -13,30 +13,25 @@
 int sockfd;
 char buffer[MAX_ENTRY_SIZE+1];
 requisicao_t req;
-// int escrita, posicao, tam_buffer;
-// char bufferChar[100];
-//
-//
-// void escrever(int posicao, char* buffer, int tam_buffer) {
-//     req.escrever = 1;
-//     req.posicao = posicao;
-//     req.tam_buffer = tam_buffer;
-//     write(sockfd, &req, sizeof(requisicao_t));
-//     write(sockfd, buffer, tam_buffer * sizeof(char));
-// }
-//
-// void ler(int posicao, int tam_buffer) {
-//     req.escrever = 0;
-//     req.posicao = posicao;
-//     req.tam_buffer = tam_buffer;
-//     write(sockfd, &req, sizeof(requisicao_t));
-//     // le
-//     memset(bufferChar, '\0', sizeof(bufferChar));
-//     read(sockfd, &bufferChar, tam_buffer);
-//     printf("%s%c\n", bufferChar, '\0');
-// }
 
+void escrever(int posicao, int tam_buffer) {
+    req.escrever = 1;
+    req.posicao = posicao;
+    req.tam_buffer = tam_buffer;
+    write(sockfd, &req, sizeof(requisicao_t));
+    write(sockfd, buffer, tam_buffer * sizeof(char));
+}
 
+void ler(int posicao, int tam_buffer) {
+    req.escrever = 0;
+    req.posicao = posicao;
+    req.tam_buffer = tam_buffer;
+    write(sockfd, &req, sizeof(requisicao_t));
+    // limpa o buffer e le o conteudo do socket
+    memset(buffer, '\0', sizeof(buffer));
+    read(sockfd, &buffer, tam_buffer);
+    printf("%s%c\n", buffer, '\0');
+}
 
 int main() {
     struct sockaddr_in address;
@@ -54,7 +49,7 @@ int main() {
     printf("addr: %s port: %hd\n", addr, address.sin_port);
     fclose(configFile);
 
-    int escrita;
+    int escrita, posicao, tam_buffer;
     while (1) {
         sockfd = socket(AF_INET, SOCK_STREAM, 0);   // tcp
         result = connect(sockfd, (struct sockaddr *)&address, len);
@@ -67,26 +62,17 @@ int main() {
         scanf("%d", &escrita);
         req.escrever = escrita;
         printf("Posicao: ");
-        scanf("%d", &req.posicao);
-        printf("Tamanho do buffer seguido da Mensagem: ");
-        scanf("%d ", &req.tam_buffer);
+        scanf("%d", &posicao);
+        printf("Tamanho do buffer e a mensagem(caso leitura entre 0): ");
+        scanf("%d ", &tam_buffer);
         if (escrita == 1) {
             fgets(buffer, MAX_ENTRY_SIZE, stdin);
-            // scanf("%100[^\n]", buffer);
-            printf("\n");
+            // scanf("%"MAX_ENTRY_SIZE"[^\n]", buffer);
+            escrever(posicao, tam_buffer);
+        } else {
+            ler(posicao, tam_buffer);
         }
 
-        // escreve a requisicao no socket
-        write(sockfd, &req, sizeof(requisicao_t));
-        // se for escrita, escreve o buffer
-        if (escrita == 1) {
-            write(sockfd, &buffer, req.tam_buffer * sizeof(char));
-        // se for leitura, limpa o buffer e escreve a resposta no buffer
-        } else {
-            memset(buffer, '\0', sizeof(buffer));
-            read(sockfd, &buffer, req.tam_buffer);
-            printf("%s\n", buffer);
-        }
 
         close(sockfd);
     }
